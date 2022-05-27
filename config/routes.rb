29efa5bfe02a6651f8cd5e8b0491f76c7e09c218
Adrien Larbouillat <ala@acoservices.eu>
2022-05-27 15:32:27 +0200

    Back end done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
-    resources :list, only: %i[index show new create] do
-      resources :bookmark, only: %i[new create destroy]
+    resources :lists, only: %i[index show new create] do
+      resources :bookmarks, only: %i[new create destroy]
     end
   end
 end
